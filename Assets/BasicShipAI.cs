993fd965200c57f58c3ea3f3536966e8c7145aa0
KTZHANG <ktzhang@sfu.ca>
2024-08-27 16:53:18 -0700

    add: whisper.unity

--- Assets/BasicShipAI.cs
+++ Assets/BasicShipAI.cs
@@ -5,7 +5,6 @@ using Zappar;
 
 public class BasicShipAI : MonoBehaviour
 {
-    public ZapparInstantTrackingTarget_EXT ZT;
     public float speed = 0.01f;
     public int Damage = 100;
     // Start is called before the first frame update
@@ -17,14 +16,5 @@ public class BasicShipAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (ZT.m_minZDistance > 0)
-        {
-            ZT.m_minZDistance -= speed;
-            ZT.m_maxZDistance -= speed;
-        }
-        else {
-            GameSettings.Health -= Damage;
-            Destroy(this);
-        }
     }
 }
